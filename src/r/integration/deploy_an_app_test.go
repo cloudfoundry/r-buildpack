029ea142a62ed0c13bb705d12ab76eb86e207e62
Jackson Feeny <jacksonfeeny@gmail.com>
2018-02-07 11:22:21 -0500

    Add integration scaffolding [#154642431]
    Signed-off-by: Dave Goddard <dave@goddard.id.au>
    Signed-off-by: Jackson Feeny <jacksonfeeny@gmail.com>

---
+++ src/r/integration/deploy_an_app_test.go
@@ -0,0 +1,33 @@
+package integration_test
+
+import (
+	"path/filepath"
+
+	"github.com/cloudfoundry/libbuildpack/cutlass"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("CF R Buildpack", func() {
+	var app *cutlass.App
+
+	AfterEach(func() {
+		if app != nil {
+			app.Destroy()
+		}
+		app = nil
+	})
+
+	Context("with a simple R app", func() {
+
+		BeforeEach(func() {
+			app = cutlass.New(filepath.Join(bpDir, "fixtures", "simple"))
+			app.Buildpacks = []string{"r_buildpack"}
+		})
+
+		It("Logs R buildpack version", func() {
+			Expect(app.Push()).ToNot(Succeed())
+			Expect(app.ConfirmBuildpack(buildpackVersion)).To(Succeed())
+		})
+	})
+})
