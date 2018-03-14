2d121258c7a02b9442f81fc12ce3024d7f9931c9
Venkatesh Kinthali <venkatesh.kinthali@jpmorgan.com>
2018-03-14 08:31:10 +0000

    Support for Vendoring packages in Application for offline environments

--- src/r/integration/install_packages_test.go
+++ src/r/integration/install_packages_test.go
@@ -35,4 +35,40 @@ var _ = Describe("CF R Buildpack", func() {
 			Eventually(app.Stdout.String).Should(ContainSubstring("HELLO WORLD"))
 		})
 	})
+
+        Context("with the vendored stringr package", func() {
+
+                BeforeEach(func() {
+                        app = cutlass.New(filepath.Join(bpDir, "fixtures", "simple_packages_vendored"))
+                        app.Disk = "1028M"
+                        app.Memory = "1028M"
+
+                })
+
+                It("Installs stringr successfully", func() {
+                        Expect(app.Push()).ToNot(Succeed())
+                        Expect(app.ConfirmBuildpack(buildpackVersion)).To(Succeed())
+
+                        Eventually(app.Stdout.String).Should(ContainSubstring("STRINGR INSTALLED SUCCESSFULLY"))
+                        Eventually(app.Stdout.String).Should(ContainSubstring("Deleting vendored packages after installation"))
+                })
+        })
+
+	Context("with the source missing for stringr package", func() {
+
+                BeforeEach(func() {
+                        app = cutlass.New(filepath.Join(bpDir, "fixtures", "simple_packages_nosource"))
+                        app.Disk = "1028M"
+                        app.Memory = "1028M"
+
+                })
+
+                It("stringr installation fails", func() {
+                        Expect(app.Push()).ToNot(Succeed())
+                        Expect(app.ConfirmBuildpack(buildpackVersion)).To(Succeed())
+
+                        Eventually(app.Stdout.String).Should(ContainSubstring("No source found for installing packages"))
+                        Eventually(app.Stdout.String).ShouldNot(ContainSubstring("STRINGR INSTALLED SUCCESSFULLY"))
+                })
+        })
 })
