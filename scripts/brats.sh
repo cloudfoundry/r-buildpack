6925eb295ed74b197565c8e37e1d972cf639d6fe
Ryan Moran <rmoran@vmware.com>
2021-05-20 14:10:02 -0700

    Bootstraps with github-config

--- scripts/brats.sh
+++ scripts/brats.sh
@@ -1,13 +1,33 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
-cd "$( dirname "${BASH_SOURCE[0]}" )/.."
-source .envrc
-./scripts/install_tools.sh
+set -e
+set -u
+set -o pipefail
 
-GINKGO_NODES=${GINKGO_NODES:-3}
-GINKGO_ATTEMPTS=${GINKGO_ATTEMPTS:-1}
-export CF_STACK=${CF_STACK:-cflinuxfs3}
+ROOTDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
+readonly ROOTDIR
 
-echo "There are no brats tests to run"
+source "${ROOTDIR}/.envrc"
 
+function main() {
+  local src
+  src="$(find "${ROOTDIR}/src" -mindepth 1 -maxdepth 1 -type d )"
+
+  if [[ ! -d "${src}/brats" ]]; then
+    echo "There are no brats tests to run"
+    exit 0
+  fi
+
+  "${ROOTDIR}/scripts/install_tools.sh"
+
+  echo "Run Buildpack Runtime Acceptance Tests"
+  CF_STACK="${CF_STACK:-cflinuxfs3}" \
+    ginkgo \
+      -r \
+      -mod vendor \
+      --flakeAttempts "${GINKGO_ATTEMPTS:-2}" \
+      -nodes "${GINKGO_NODES:-3}" \
+        "${src}/brats"
+}
+
+main "${@:-}"
